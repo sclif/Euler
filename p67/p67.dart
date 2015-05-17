61ca9d7bbf8ef81cd93aa5c207b79fbf45c2490c
sclif <paul.pazniak@gmail.com>
2015-05-18 00:24:37 +1200

    p67

---
+++ p67/p67.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+import 'dart:math';
+
+main(){
+  String path = r'd:\downloads\p067_triangle.txt';
+  // String path = r'd:\downloads\p18_triangle.txt';
+  File f = new File(path);
+  var strings = f.readAsLinesSync();
+  List<List<int>> tri = [];
+  strings.forEach((s){
+    List<int> row = [];
+    var numbers = s.split(" ");
+    numbers.forEach((n) => row.add(int.parse(n)));
+    tri.add(row);
+  });
+
+  int max = 0;
+  for(int i = 0; i < tri.length; i++){
+    var romb = buildRomb(tri, i);
+    // print(romb);
+    if(romb[tri.length - 1][0] > max){
+      max = romb[tri.length - 1][0];
+    }
+  }
+  print(max);
+}
+
+List<List<int>> buildRomb(List<List<int>> tri, int start){
+  List<List<int>> romb = [];//new List<List<int>>.from(tri);
+  // List<int> range = [start];
+  var r = new List<int>.filled(tri.length, 0);
+  r[start] = tri[tri.length - 1][start];
+  romb.add(r);
+  int end = min(start + 1, tri.length - 1);
+  start = max(start - 1, 0);
+  // print('$r $start $end');
+  for(int i = tri.length - 2; i >= 0; i--){
+    var row = tri[i];
+    var newrow = new List<int>.filled(i + 1, 0);
+    for(int j = start; j < end; j++){
+      var exrow = romb[romb.length - 1];
+
+      int v = row[j] + max(exrow[j], exrow[j + 1]);
+      newrow[j] = v;
+    }
+    // print(newrow);
+    romb.add(newrow);
+    start = max(start - 1, 0);
+    end = min(end, row.length - 1);
+  }
+  return romb;
+}
